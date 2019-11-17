deae1f8cfb9d99fe49a1dba0b7ef5b1e62651c02
Dũng <dungbv2201@gmail.com>
2019-11-17 12:37:59 +0700

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                   Hello guys
+                   Hi Quyên!
                 </div>
 
                 <div class="links">
