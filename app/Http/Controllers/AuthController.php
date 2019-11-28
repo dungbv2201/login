a29c26e7fceab2a629a4c73fcb48845027613a0e
Dũng <dungbv2201@gmail.com>
2019-11-29 04:20:41 +0700

    logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -37,4 +37,12 @@ class AuthController extends Controller
     public function getUserInfo(Request $request){
         return response()->json($request->user());
     }
+
+    public function logout(Request $request)
+    {
+        $request->user()->token()->revoke();
+        return response()->json([
+            'message' => 'Successfully logged out'
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,5 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => 'auth:api'], function() {
     Route::get('users-info', 'AuthController@getUserInfo');
+    Route::post('logout', 'AuthController@logout');
 });
 Route::post('login', 'AuthController@login');
