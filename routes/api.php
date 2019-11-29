a1cd56a4b3cc7a59e809ff86431727b2d2f33974
Dũng <dungbv2201@gmail.com>
2019-11-29 10:00:06 +0700

    rm cors

--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group(['middleware' => 'cors'], function () {
-    Route::group(['middleware' => 'auth:api'], function () {
-        Route::get('users-info', 'AuthController@getUserInfo');
-        Route::post('logout', 'AuthController@logout');
-    });
-    Route::post('login', 'AuthController@login');
+//Route::group(['middleware' => 'cors'], function () {
+//
+//});
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::get('users-info', 'AuthController@getUserInfo');
+    Route::post('logout', 'AuthController@logout');
 });
+Route::post('login', 'AuthController@login');
